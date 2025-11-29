cc663f27899824fc16d09c4e699f2bd5fdcfe60a
Özlen Yudum Besleme <yudum.besleme@hubx.co>
2025-11-29 18:28:46 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/myrefereeai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.myrefereeai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/splash.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 2 saniyede soldan sağa dolacak
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 6),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.forward();
+
+    // Doldurma bitince -> ana sayfaya geçebilirsin
+    // _animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     Navigator.pushReplacement(...);
+    //   }
+    // });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Spacer(),
+
+            Center(
+              child: Image.asset(
+                'assets/images/myrefereeai_logo.png',
+                width: 400,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            const Spacer(),
+
+            /// YÜZDESEL DOLAN LOADER
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: SizedBox(
+                width: 180,
+                height: 8,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(999),
+                  child: AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) {
+                      return LinearProgressIndicator(
+                        value: _animation.value, // 0 → 1 şekilde dolar
+                        minHeight: 4,
+                        backgroundColor: Colors.black.withOpacity(0.08),
+                        valueColor: const AlwaysStoppedAnimation<Color>(
+                          Colors.black,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
